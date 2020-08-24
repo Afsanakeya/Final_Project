4756af9c0720079891ff05a8dcebbe1905715905
afsanakeya <afsanakeya@localhost>
2020-08-23 22:38:52 -0400

    my new commit

---
+++ src/page_helper/locators_values.java
@@ -0,0 +1,28 @@
+package page_helper;
+
+public class locators_values {
+	
+		public String signInLinkLoc 		= "//*[@id=\"header-signin-link\"]"; // xpath
+		public String emailLoc 				= "//*[@id=\"login-username\"]"; 
+		public String nextButtonLoc			= "#login-signin";//css 
+		public String passwordLoc			= "//*[@id=\"login-passwd\"]";
+		public String nextButtonloc		    = "//*[@id=\"login-signin\"]";
+		public String myActualNameLoc	    = "//*[@id=\"header-profile-button\"]/div[2]";
+	    public String inlinelocator         ="//*[@id=\"password-challenge\"]/form/p";
+		public String Nothanksloc           ="//*[@id=\"mega-banner-close\"]";
+		public String Composelink           ="//*[@id=\"app\"]/div[2]/div/div[1]/nav/div/div[1]/a";
+		public String To_link               ="//*[@id=\"message-to-field\"]";
+		public String Subject_link          ="//*[@id=\"mail-app-component\"]/div[2]/div/div[1]/div[3]/div/div/input";
+		public String Mail_body             ="//*[@id=\"editor-container\"]/div[1]";
+		public String Send_button           ="//*[@id=\"mail-app-component\"]/div[2]/div/div[2]/div[2]/div/button";
+		public String sent_buttonloc        ="//*[@id=\"app\"]/div[2]/div/div[1]/nav/div/div[3]/div[3]/ul/li[5]/div/a/span[1]";
+		public String delete_loc            ="//*[@id=\"app\"]/div[7]/div/div[1]/div/div/ul/li[9]/a/span";
+		// all the values -- test data, name, email, password
+		public String emailValue 			= "keya192001@yahoo.com";
+		public String passValue				= "amayra03";
+	    public String writeTolink           ="hannan632010@yahoo.com";
+	    public String writesublink          ="hello";
+	    public String body_message          ="hello,How are you doing?";
+	    
+	    
+}
